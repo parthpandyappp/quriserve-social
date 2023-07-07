f56886b6a87d5e478e6bb11b05584eecb562af75
parthpandyappp <parthpandyappp@gmail.com>
2023-07-08 01:26:04 +0530

    fix: undo redundant cors configs

--- next.config.js
+++ next.config.js
@@ -3,19 +3,7 @@ const nextConfig = {
     images: {
         domains: ['cdn.quriverse.com'],
     },
-    async headers() {
-        return [
-            {
-                source: "/csr",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "false" },
-                    { key: "Access-Control-Allow-Origin", value: "https://api.quriverse.com" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
-            }
-        ]
-    }
+
 }
 
 module.exports = nextConfig
