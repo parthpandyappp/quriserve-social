b440d9d15c6da300ebf9eda1b2e2211a9e23fcc9
parthpandyappp <parthpandyappp@gmail.com>
2023-07-08 01:15:43 +0530

    fix: CORS config in nextjs config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,19 @@ const nextConfig = {
     images: {
         domains: ['cdn.quriverse.com'],
     },
-    async rewrites() {
+    async headers() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'https://api.example.com/:path*',
-            },
+                source: "https://api.quriverse.com/api/public-base/posts/eb2f99e2-3940-4d4a-998c-55d1a6c3e0bc",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "false" },
+                    { key: "Access-Control-Allow-Origin", value: "https://example.com" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                ]
+            }
         ]
-    },
+    }
 }
 
 module.exports = nextConfig
